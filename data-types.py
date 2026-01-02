9a501deb035742b977c5764daa5b2a35deb65598
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2026-01-02 20:20:28 +0530

    data-types.py

--- data-types.py
+++ data-types.py
@@ -1,4 +1,7 @@
 data = [10, 3.5, "DS", True, [1,2], (1,2), {"a":1}, {1,2}, None]
 
+mutable_types = (list, dict, set, bytearray)
+
 for i in data: 
-    print(i, "->", type(i).__name__)
+    is_mutable = isinstance(i, mutable_types)
+    print(i, "->", type(i).__name__, "| Mutable: ", is_mutable)
