4a5540dc9e22bb8b6358325725ced99ef8084bbd
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2026-01-03 16:31:35 +0530

    data-types.py

--- data-types.py
+++ data-types.py
@@ -1,6 +1,14 @@
-data = [10, 3.5, "DS", True, [1,2], (1,2), {"a":1}, {1,2}, None, bytearray(b"abc"), bytes(b"abc"), frozenset({1, 2})]
+class CustomObject:
+    def __init__(self, value):
+        self.value = value
 
-mutable_types = (list, dict, set, bytearray)
+    def __repr__(self):
+        return f"{self.value}"
+
+
+data = [10, 3.5, 2 + 5j, "DS", True, [1,2], (1,2), {"a":1, "b":2, "c":3}, {1,2}, None, bytearray(b"abc"), bytes(b"abc"), frozenset({1, 2}), CustomObject(10)]
+
+mutable_types = (list, dict, set, bytearray, CustomObject)
 
 for i in data: 
     is_mutable = isinstance(i, mutable_types)
