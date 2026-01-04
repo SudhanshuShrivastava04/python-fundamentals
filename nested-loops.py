fdf7a90f498abb0a8c59176d2b6ebc82df5c1227
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2026-01-04 20:25:54 +0530

    nested-loops.py

--- nested-loops.py
+++ nested-loops.py
@@ -3,3 +3,26 @@ print("Enter Length & Breadth of Butterly!\n")
 l = int(input("Height: "))
 b = int(input("Breadth: "))
 
+for i in range(1, l+1):
+    for j in range(i):
+        print("*", end="")
+
+    for j in range(2*(b - i)):
+        print(" ", end="")
+
+    for j in range(i):
+        print("*", end="")
+    
+    print()
+
+for i in range(l, 0, -1):
+    for j in range(i):
+        print("*", end="")
+
+    for j in range(2*(b - i)):
+        print(" ", end="")
+
+    for j in range(i):
+        print("*", end="")
+
+    print()
