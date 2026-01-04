3c36221eea3b0eeae1b8fd5aef33cd68176c8b2f
Sudhanshu Shrivastava <211230053@nitdelhi.ac.in>
2026-01-04 17:39:21 +0530

    guess-the-number.py

--- guess-the-number.py
+++ guess-the-number.py
@@ -5,14 +5,16 @@ high = int(input("Enter upperbound: "))
 
 print("\nThink of a number between", low, " and ", high)
 
+counter = 0
 while True:
     guess = (low + high) // 2 
+    counter += 1
     print(f"\nIs {guess} is the correct guess?")
 
     response = input("Enter (h)igher, (l)ower or (c)orrect: ").lower()
 
     if response == "c":
-        print(f"\nYay, {guess} is a correct guess?")
+        print(f"\nYay, {guess} is a correct guess and it took {counter} iterations")
         break 
     elif response == "h":
         low = guess + 1 
